d13d2c7850067ce631ff2907834abcb0759b0d9e
Darius Sas <darius.sas@outlook.com>
2018-12-28 16:19:03 +0100

    Implementing as tracking

--- src/main/java/org/rug/tracker/ASTracker.java
+++ src/main/java/org/rug/tracker/ASTracker.java
@@ -1,4 +1,36 @@
 package org.rug.tracker;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.rug.data.EdgeLabel;
+import org.rug.data.VertexLabel;
+
+import java.util.List;
+import java.util.Map;
+
 public class ASTracker {
+
+    public static void trackAS(Graph graphV1, Graph graphV2){
+        // Forward pass
+        GraphTraversalSource g1 = graphV1.traversal();
+        GraphTraversalSource g2 = graphV2.traversal();
+
+        Map<String, List<Vertex>> smellGroups = (Map<String, List<Vertex>>)(Map<?,?>)g1.V().hasLabel(VertexLabel.SMELL.toString()).group().by("smellType").next();
+
+        smellGroups.forEach((type, vertices) -> {
+            switch (type) {
+                case "cyclicDependency":
+                    Vertex star = g1.V(vertices).in(EdgeLabel.PARTOFSTAR.toString()).next();
+
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        smellGroups.clear();
+
+        // Backward pass
+    }
 }
---
+++ src/test/java/org/rug/tracker/ASTrackerTest.java
@@ -0,0 +1,19 @@
+package org.rug.tracker;
+
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.junit.jupiter.api.Test;
+import org.rug.data.SmellGraphFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ASTrackerTest {
+
+    @Test
+    void trackAS() {
+        Graph g1 = SmellGraphFactory.extendWithDummyNodes(SmellGraphFactory.createStarSmell(10));
+        Graph g2 = SmellGraphFactory.simpleExtendEvolution(g1, 5);
+
+        ASTracker.trackAS(g1, g2);
+    }
+
+}
