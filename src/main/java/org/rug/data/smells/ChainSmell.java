a79fa44310f5555f8d861bc4f9986e6e638ce130
Darius Sas <darius.sas@outlook.com>
2019-01-08 16:30:30 +0100

    Push

--- src/main/java/org/rug/data/smells/ChainSmell.java
+++ src/main/java/org/rug/data/smells/ChainSmell.java
@@ -2,8 +2,6 @@ package org.rug.data.smells;
 
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 
-import java.util.Set;
-
 public class ChainSmell extends CDSmell {
 
     protected ChainSmell(Vertex smell) {
