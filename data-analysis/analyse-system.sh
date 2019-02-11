2092d4f5bf47de8f01655905851ee1852b07503f
Darius Sas <darius.sas@outlook.com>
2019-02-11 14:24:22 +0100

    Added analysis for multiple systems

--- data-analysis/analyse-system.sh
+++ data-analysis/analyse-system.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -x
+# #!/bin/sh -x
 
 TRACKAS_JAR="../target/trackas/trackas-0.1.jar"
 
-usage() {
-    echo "Usage:    analyse-system <project-name> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
+usage_single() {
+    echo "Usage:    analyse-single -p <project-name> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
+    exit
+}
+
+usage_multiple(){
+    echo "Usage:    analyse-multiple -m <master-directory> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
     exit
 }
 
@@ -11,70 +16,121 @@ trackas(){
     java -jar $TRACKAS_JAR $@
 }
 
-PROJECT=""
-RUN_ARCAN=""
-RUN_TRACKER=false
-RECOMPILE_TRACKER=false
-NON_CONSEC_VERS=""
+analyse_single(){
+    PROJECT=""
+    RUN_ARCAN=""
+    RUN_TRACKER=false
+    RECOMPILE_TRACKER=false
+    NON_CONSEC_VERS=""
 
+    while [ "$1" != "" ]; do
+    case $1 in
+        -p | --project )        shift
+                                PROJECT=$1
+                                ;;
+        -o | --outdir )         shift
+                                OUTPUTDIR=$1
+                                ;;
+        -rT | --run-tracker )   RUN_TRACKER=true
+                                ;;
+        -rA | --run-arcan )     RUN_ARCAN="-rA"
+                                ;;
+        -tC | --track-consec-only )  NON_CONSEC_VERS="-dNC"
+                                ;;
+        -c  | --recompile-tracker ) RECOMPILE_TRACKER=true
+                                ;;
+        -h | --help )           usage_single
+                                exit
+                                ;;
+        * )                     usage_single
+                                exit 1
+    esac
+    shift
+    done
 
-while [ "$1" != "" ]; do
-  case $1 in
-    -p | --project )        shift
-                            PROJECT=$1
-                            ;;
-    -rT | --run-tracker )   RUN_TRACKER=true
-                            ;;
-    -rA | --run-arcan )     RUN_ARCAN="-rA"
-                            ;;
-    -tC | --track-consec-only )  NON_CONSEC_VERS="-dNC"
-                            ;;
-    -c  | --recompile-tracker ) RECOMPILE_TRACKER=true
-                            ;;
-    -h | --help )           usage
-                            exit
-                            ;;
-    * )                     usage
-                            exit 1
-  esac
-  shift
-done
+    if [ -z $PROJECT ] ; then
+        echo "Please provide a project name."
+        usage_single
+    fi
 
-INPUTDIR="../test-data/output/arcanOutput/$PROJECT/"
-OUTPUTDIR="../test-data/output"
+    if [ -z $OUTPUTDIR ] ; then
+        echo "Please provide an output directory."
+        usage_single
+    fi
 
-if [ $RUN_TRACKER = true ] ; then
-    if [ $RECOMPILE_TRACKER = true ] ; then 
-        oldir=$(pwd)
-        cd ..
-        echo "Compiling tracker..."
-        mvn package > /dev/null
-        if [ $? -eq 0 ] ; then
-            echo "Build successful."
-        else
-            echo "Build failed. Exiting..."
+    INPUTDIR="$OUTPUTDIR/arcanOutput/$PROJECT/"
+
+    if [ $RUN_TRACKER = true ] ; then
+        if [ $RECOMPILE_TRACKER = true ] ; then 
+            oldir=$(pwd)
+            cd ..
+            echo "Compiling tracker..."
+            mvn package > /dev/null
+            if [ $? -eq 0 ] ; then
+                echo "Build successful."
+            else
+                echo "Build failed. Exiting..."
+                exit
+            fi
+            cd $oldir
+        fi
+        trackas -p $PROJECT -i $INPUTDIR -o $OUTPUTDIR -pC -pS $RUN_ARCAN $NON_CONSEC_VERS
+        if [ $? -ne 0 ] ; then
+            echo "Tracking failed. Exiting..."
             exit
         fi
-        cd $oldir
     fi
-    trackas -p $PROJECT -i $INPUTDIR -o $OUTPUTDIR -pC -pS $RUN_ARCAN $NON_CONSEC_VERS
-    if [ $? -ne 0 ] ; then
-        echo "Tracking failed. Exiting..."
-        exit
+
+    OUTPUTDIR=$OUTPUTDIR/trackASOutput/$PROJECT
+
+    if [ -z $NON_CONSEC_VERS ] ; then
+        SUFFIX="nonConsec"
+    else
+        SUFFIX="consecOnly"
     fi
-fi
 
-OUTPUTDIR=$OUTPUTDIR/trackASOutput/$PROJECT
+    SIMILARITY_SCORES_FILE=$OUTPUTDIR/similarity-scores-$SUFFIX.csv
+    SMELL_CHARACTERISTICS_FILE=$OUTPUTDIR/smell-characteristics-$SUFFIX.csv
 
-if [ -z $NON_CONSEC_VERS ] ; then
-    SUFFIX="nonConsec"
-else
-    SUFFIX="consecOnly"
-fi
+    Rscript jaccard-linking.r $SIMILARITY_SCORES_FILE $OUTPUTDIR/smell-similarity-matrices-$SUFFIX.pdf
 
-SIMILARITY_SCORES_FILE=$OUTPUTDIR/similarity-scores-$SUFFIX.csv
-SMELL_CHARACTERISTICS_FILE=$OUTPUTDIR/smell-characteristics-$SUFFIX.csv
+    R -e "e<-new.env();e[['project']]<-'$PROJECT';e[['type']]<-'$SUFFIX';rmarkdown::render('as-history-in-system.Rmd', output_file='$OUTPUTDIR/as-history-in-system-$SUFFIX.nb.html',envir=e)"
+}
 
-Rscript jaccard-linking.r $SIMILARITY_SCORES_FILE $OUTPUTDIR/smell-similarity-matrices-$SUFFIX.pdf
+analyse_multiple(){
 
-R -e "e<-new.env();e[['project']]<-'$PROJECT';e[['type']]<-'$SUFFIX';rmarkdown::render('as-history-in-system.Rmd', output_file='$OUTPUTDIR/as-history-in-system-$SUFFIX.nb.html',envir=e)"
+    while [ "$1" != "" ]; do
+    case $1 in
+        -m | --master-dir )     shift
+                                MASTERDIR=$1
+                                ;;
+        -o | --outdir )         shift
+                                OUTPUTDIR=$1
+                                ;;
+        -rT | --run-tracker )   RUN_TRACKER="-rT"
+                                ;;
+        -rA | --run-arcan )     RUN_ARCAN="-rA"
+                                ;;
+        -tC | --track-consec-only )  CONSECONLY="-tC"
+                                ;;
+        -c  | --recompile-tracker ) RECOMPILE_TRACKER="-c"
+                                ;;
+        -h | --help )           usage_multiple
+                                exit
+                                ;;
+        * )                     usage_multiple
+                                exit 1
+    esac
+    shift
+    done
+
+    for project_dir in $MASTERDIR/*/
+    do
+        project_dir=${project_dir%*/} 
+        PROJECT=$(basename $project_dir)
+        echo "analyse_single -p $PROJECT -o $OUTPUTDIR $RUN_TRACKER $RUN_ARCAN $NON_CONSEC_VERS $RECOMPILE_TRACKER"
+        analyse_single -p $PROJECT -o $OUTPUTDIR $RUN_TRACKER $RUN_ARCAN $CONSECONLY $RECOMPILE_TRACKER
+    done
+}
+
+analyse_multiple $@
