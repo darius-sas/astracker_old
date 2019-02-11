d22a8608709799cdf9c5af131e3b66b59ff71016
Darius Sas <darius.sas@outlook.com>
2019-02-11 15:05:45 +0100

    Added download script for qc

---
+++ data-analysis/download-qc-gdrive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gdown.pl/gdown.pl https://drive.google.com/open?id=13BYkWemF5S6M8E0CPXt3Ko9ymd2I8CHh qce-pt1.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1pvkdlthhwGm7IYYZCXCTy7xBUn_2NHB2 qce-pt2.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=165U0rFEdD5uelIPe8xUefdOg0PWhXQ0_ qce-pt3.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1TZraG4CT-3POR_9psncjmsSxrbE9hN_L qce-pt4.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1lPRYisYhNOnN6EvFB1pePFPbJBN0IpGo qce-pt5.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1c8RoI1bm1Ip9Dzp7yvATDIbBlHiZXu1q qce-pt6.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1PqsyD80LuInQSVs-mcVV8SZsruDlvVJz qce-pt7.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1fXsEVAUS7NFYTM2y0iDXXOqXUFo5j7oH qce-pt8.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1mF0VT78DYVOcBD-MjUYpmxAhyi07ytH_ qce-pt9.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1sTo33XRsucCUinFAC8daHf_YW_pZYBdG qcr-pt1.tar
+gdown.pl/gdown.pl https://drive.google.com/open?id=1L5y9YfCTVkS58wucKnlIHB-8E-rH5Xot qcr-pt2.tar
