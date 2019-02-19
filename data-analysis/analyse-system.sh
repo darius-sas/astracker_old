e3cf8c9c0f130c281e6bb5a29a8fb16d2752b78e
Darius Sas <darius.sas@outlook.com>
2019-02-19 16:30:37 +0100

    Fixed analysis of multiple systems

--- data-analysis/analyse-system.sh
+++ data-analysis/analyse-system.sh
@@ -1,50 +1,72 @@
 #!/bin/bash
 
 TRACKAS_JAR="../target/trackas/trackas-0.1.jar"
+ANALYSE_NOTEBOOK="./as-history-in-system.Rmd"
+
+MASTERDIR=""
+PROJECT=""
+RUN_ARCAN=""
+RUN_TRACKER=""
+RECOMPILE_TRACKER=""
+NON_CONSEC_VERS="-dNC"
+OUTPUTDIR=""
+
+parse_args(){
+
+    while [ "$1" != "" ]; do
+    case $1 in
+        -m | --master-dir )     shift
+                                MASTERDIR=$1
+                                ;;
+        -o | --outdir )         shift
+                                OUTPUTDIR=$1
+                                ;;
+        -rT | --run-tracker )   RUN_TRACKER="-rT"
+                                ;;
+        -p | --project )        shift
+                                PROJECT=$1
+                                ;;
+        -rT | --run-tracker )   RUN_TRACKER="-rT"
+                                ;;
+        -rA | --run-arcan )     RUN_ARCAN="-rA"
+                                ;;
+        -tNC | --track-non-consec ) NON_CONSEC_VERS=""
+                                ;;
+        -c  | --recompile-tracker ) RECOMPILE_TRACKER="-c"
+                                ;;
+        -h | --help )           usage_single
+                                usage_multiple
+                                ;;
+    esac
+    shift
+    done
+}
 
 usage_single() {
-    echo "Usage:    analyse-single -p <project-name> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
+    echo "Usage:\n    analyse-single -p <project-name> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
 }
 
 usage_multiple(){
-    echo "Usage:    analyse-multiple -m <master-directory> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
+    echo "Usage:\n    analyse-multiple -m <master-directory> -o <out-directory> [-rA|-run-Arcan][-rT|-run-Tracker][-c|--recompile-tracker][-tC|-track-consec-only][-h|--help]"
 }
 
 trackas(){
     java -jar $TRACKAS_JAR $@
 }
 
-analyse_single(){
-    PROJECT=""
-    RUN_ARCAN=""
-    RUN_TRACKER=false
-    RECOMPILE_TRACKER=false
-    NON_CONSEC_VERS=""
+# Synopsis: similarity_score <input-scores-file> <output-pdf-file>
+similarity_score(){
+    Rscript jaccard-linking.r $1 $2
+}
 
-    while [ "$1" != "" ]; do
-    case $1 in
-        -p | --project )        shift
-                                PROJECT=$1
-                                ;;
-        -o | --outdir )         shift
-                                OUTPUTDIR=$1
-                                ;;
-        -rT | --run-tracker )   RUN_TRACKER=true
-                                ;;
-        -rA | --run-arcan )     RUN_ARCAN="-rA"
-                                ;;
-        -tC | --track-consec-only )  NON_CONSEC_VERS="-dNC"
-                                ;;
-        -c  | --recompile-tracker ) RECOMPILE_TRACKER=true
-                                ;;
-        -h | --help )           usage_single
-                                return
-                                ;;
-        * )                     usage_single
-                                return
-    esac
-    shift
-    done
+# Synopsis: notebook <project-name> <type> <score-file> <characteristics-file> <output-file>
+notebook(){
+    R -e "e<-new.env();e[['project']]<-'$1';e[['type']]<-'$2';e[['similarity_scores_file']]<-'$3';e[['characteristics_file']]<-'$4';rmarkdown::render('$ANALYSE_NOTEBOOK', output_file='$5',envir=e)"
+}
+
+analyse_single(){
+
+    parse_args $@
 
     if [ -z $PROJECT ] ; then
         echo "Please provide a project name."
@@ -60,8 +82,8 @@ analyse_single(){
 
     INPUTDIR="$OUTPUTDIR/arcanOutput/$PROJECT/"
 
-    if [ $RUN_TRACKER = true ] ; then
-        if [ $RECOMPILE_TRACKER = true ] ; then 
+    if [[ $RUN_TRACKER == "-rT" ]] ; then
+        if [[ $RECOMPILE_TRACKER == "-c" ]] ; then
             oldir=$(pwd)
             cd ..
             echo "Compiling tracker..."
@@ -81,7 +103,7 @@ analyse_single(){
         fi
     fi
 
-    OUTPUTDIR=$OUTPUTDIR/trackASOutput/$PROJECT
+    OUTPUTDIR_PROJECT=$OUTPUTDIR/trackASOutput/$PROJECT
 
     if [ -z $NON_CONSEC_VERS ] ; then
         SUFFIX="nonConsec"
@@ -89,48 +111,31 @@ analyse_single(){
         SUFFIX="consecOnly"
     fi
 
-    SIMILARITY_SCORES_FILE=$OUTPUTDIR/similarity-scores-$SUFFIX.csv
-    SMELL_CHARACTERISTICS_FILE=$OUTPUTDIR/smell-characteristics-$SUFFIX.csv
+    SIMILARITY_SCORES_FILE=$OUTPUTDIR_PROJECT/similarity-scores-$SUFFIX.csv
+    SMELL_CHARACTERISTICS_FILE=$OUTPUTDIR_PROJECT/smell-characteristics-$SUFFIX.csv
 
-    Rscript jaccard-linking.r $SIMILARITY_SCORES_FILE $OUTPUTDIR/smell-similarity-matrices-$SUFFIX.pdf
-
-    R -e "e<-new.env();e[['project']]<-'$PROJECT';e[['type']]<-'$SUFFIX';rmarkdown::render('as-history-in-system.Rmd', output_file='$OUTPUTDIR/as-history-in-system-$SUFFIX.nb.html',envir=e)"
+    similarity_score $SIMILARITY_SCORES_FILE $OUTPUTDIR_PROJECT/smell-similarity-matrices-$SUFFIX.pdf
+    notebook $PROJECT $SUFFIX $SIMILARITY_SCORES_FILE $SMELL_CHARACTERISTICS_FILE "$OUTPUTDIR_PROJECT/as-history-in-system-$SUFFIX.nb.html"
 }
 
 analyse_multiple(){
 
+    parse_args $@
 
-
-    while [ "$1" != "" ]; do
-    case $1 in
-        -m | --master-dir )     shift
-                                MASTERDIR=$1
-                                ;;
-        -o | --outdir )         shift
-                                OUTPUTDIR=$1
-                                ;;
-        -rT | --run-tracker )   RUN_TRACKER="-rT"
-                                ;;
-        -rA | --run-arcan )     RUN_ARCAN="-rA"
-                                ;;
-        -tC | --track-consec-only )  CONSECONLY="-tC"
-                                ;;
-        -c  | --recompile-tracker ) RECOMPILE_TRACKER="-c"
-                                ;;
-        -h | --help )           usage_multiple
-                                return
-                                ;;
-        * )                     usage_multiple
-                                return
-    esac
-    shift
-    done
+    if [[ -z $MASTERDIR || -z $OUTPUTDIR ]]
+    then
+        echo "Please provide a master and an output directory."
+        usage_multiple
+        return
+    fi
 
     for project_dir in $MASTERDIR/*/
     do
         project_dir=${project_dir%*/} 
         PROJECT=$(basename $project_dir)
-        echo "analyse_single -p $PROJECT -o $OUTPUTDIR $RUN_TRACKER $RUN_ARCAN $NON_CONSEC_VERS $RECOMPILE_TRACKER"
-        analyse_single -p $PROJECT -o $OUTPUTDIR $RUN_TRACKER $RUN_ARCAN $CONSECONLY $RECOMPILE_TRACKER
+
+        analyse_single -p $PROJECT -o $OUTPUTDIR $RUN_TRACKER $RUN_ARCAN $NON_CONSEC_VERS $RECOMPILE_TRACKER
+        RECOMPILE_TRACKER=""
     done
 }
+
