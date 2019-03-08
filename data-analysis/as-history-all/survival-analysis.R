01f7569676cd81f1dd9f8578bbbea6efab2e4352
Darius Sas <darius.sas@outlook.com>
2019-03-08 12:23:17 +0100

    Completed survival analysis and added some plots

--- data-analysis/as-history-all/survival-analysis.R
+++ data-analysis/as-history-all/survival-analysis.R
@@ -4,27 +4,26 @@ library(dplyr)
 
 
 computeSurvivalAnalysis<- function(df){
-  df.proj <- df %>% group_by(project) %>% summarise(firstVersion = sort(version)[1], lastVersion = sort(version)[length(version)])
+  df.proj <- df %>% group_by(project) %>%
+    summarise(firstVersion = version[which.min(versionPosition)], 
+              lastVersion = version[which.max(versionPosition)],
+              n.versions = max(versionPosition),
+              n.smells = length(unique(uniqueSmellID)))
   df.smel <- df %>%
     group_by(uniqueSmellID, project) %>%
-    summarise(appearedIn = sort(version)[1], deletedIn = sort(version)[length(version)])
-  df.dup <- df[!duplicated(df[, c("project", "uniqueSmellID")]), c("project", "uniqueSmellID", "smellType", "age")]
+    summarise(lastVersion = max(versionPosition))
+  df.smel <- left_join(df.smel, df.proj[,c("project","n.versions")], by="project")
+  df.smel$presentInLastVersion <- df.smel$lastVersion == df.smel$n.versions
+  
+  df.dup <- df[!duplicated(df[, c("project", "uniqueSmellID")]), c("project", "uniqueSmellID", "smellType", "age", "versionPosition")]
   df.surv <- left_join(df.smel, df.dup, by=c("project", "uniqueSmellID"))
-  df.surv <- df.surv %>% 
-    mutate(isInLastVersion = df.proj[df.proj$project==project, "lastVersion"] == as.character(deletedIn))
   
-  ggsurv <- ggsurvplot(survfit(Surv(time = age, event = isInLastVersion) ~ smellType + project, data = df.surv), 
-                       data = df.surv)
-  ggsurv$plot + facet_grid(rows=vars(project))
+  fitmodel = survfit(Surv(time = age, event = presentInLastVersion) ~ smellType + project, data = df.surv)
   
-  plots <- list()
-  i = 1
-  for (project in levels(df.surv$project)) {
-    df.tmp <- df.surv[df.surv$project==project,]
-    surv_object <- Surv(time = df.tmp$age, event = df.tmp$isInLastVersion)
-    fit1 <- survfit(surv_object ~ smellType, data = df.tmp)
-    
-    plots[[i]] <- ggsurvplot(fit1, data = df.tmp, pval = TRUE, conf.int = TRUE) + facet_wrap(~project)
-    i = i + 1
-  }
+  return(list(model=fitmodel, data = df.surv))
+}
+
+
+computeAgeDensity <- function(df){
+  
 }
