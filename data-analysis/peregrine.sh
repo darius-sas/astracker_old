ae7b6885ea062666739478b36a136d5ffffc2ceb
Darius Sas <darius.sas@outlook.com>
2019-03-11 10:25:10 +0100

    Changed configuration 3

--- data-analysis/peregrine.sh
+++ data-analysis/peregrine.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=track_as_qualitas_corpus
 #SBATCH --mail-type=ALL
-#SBATCH --time=4-01:00:00
+#SBATCH --time=3-20:00:00
 #SBATCH --mail-user=d.d.sas@rug.nl
 #SBATCH --output=job-%j.log
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=100000
+#SBATCH --mem=90000
 
 module restore trackas
 
