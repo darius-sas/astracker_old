ef58b07878b9e43f95913021bde307594af51aaf
Darius Sas <darius.sas@outlook.com>
2019-05-20 13:13:37 +0200

    Refactored analysis folder

--- data-analysis/util/merge-results.R
+++ data-analysis/util/merge-results.R
@@ -8,7 +8,7 @@ if (length(args) < 2) {
   quit()
 }
 
-merge.dataset <- function(dir, pattern){
+merge.results <- function(dir, pattern){
   projectDirs <- list.files(dir, recursive = T, pattern = pattern)
   
   df <- data.frame()
@@ -30,17 +30,28 @@ if (length(args) == 3 & !is.null(args[3])) {
   consecOnly = FALSE
 }
 
+results.dir <- args[1]
+output.dir <- args[2]
 pattern <- ifelse(consecOnly,"-consecOnly.csv", "-nonConsec.csv")
 
-sc.dataset = file.path(args[2], "dataset.csv")
-ps.dataset = file.path(args[2], "project-sizes.csv")
+# Create paths to output files
+sc.dataset = file.path(output.dir, "smells.csv")
+ps.dataset = file.path(output.dir, "projects.csv")
+cc.dataset = file.path(output.dir, "components.csv")
+af.dataset = file.path(output.dir, "affected.csv")
 
-df <- merge.dataset(args[1], paste("*smell-characteristics", pattern, sep=""))
-df.sizes <- merge.dataset(args[1], paste("*project-sizes", pattern, sep=""))
-df <- left_join(df, df.sizes, by = c("project", "version"))
+# Invoke merging and do postprocessing
+df <- merge.results(results.dir, paste("*smell-characteristics", pattern, sep=""))
+df.projects <- merge.results(results.dir, paste("*project-sizes", pattern, sep=""))
+df <- left_join(df, df.projects, by = c("project", "version"))
 df <- df %>% mutate(pageRankWeighted = ifelse(affectedComponentType=="package", 
                                               pageRankMax * nPackages, 
                                               pageRankMax * nClasses))
+df.components <- merge.results(results.dir, paste("*components-characteristics", pattern, sep = ""))
+df.affected <- merge.results(results.dir, paste("*affected-components", pattern, sep = ""))
 
+# Write the datasets to file
 write.csv(df, file = sc.dataset, row.names = F)
-write.csv(df.sizes, file = ps.dataset, row.names = F)
+write.csv(df.projects, file = ps.dataset, row.names = F)
+write.csv(df.components, file = cc.dataset, row.names = F)
+write.csv(df.affected, file = af.dataset, row.names = F)
