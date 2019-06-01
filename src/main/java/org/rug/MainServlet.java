471cfa41e6507bfb03ea2fb738091b94f4eaa561
Darius Sas <darius.sas@outlook.com>
2019-06-01 23:34:54 +0200

    Cleaned pom file and added possibility to build single jar

--- src/main/java/org/rug/MainServlet.java
+++ src/main/java/org/rug/MainServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello, World!");
+        resp.getWriter().println("Hello, World!!!");
     }
 }
