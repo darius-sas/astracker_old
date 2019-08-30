8131212642b39338fccb032ff0878805ef3b6584
dkarakaya <deniz.karakaya@nucleo.com.tr>
2019-08-30 11:15:51 +0200

    tracker test added

--- src/test/java/org/rug/tracker/ASmellTrackerTest.java
+++ src/test/java/org/rug/tracker/ASmellTrackerTest.java
@@ -24,7 +24,7 @@ class ASmellTrackerTest {
 	@Test
 	void trackTest() throws IOException {
 		var name = "pure";
-		var dir = "./arcanCppOutput/pure";
+		var dir = "./arcanCppOutput/"+name;
 		ProjectCPP pr = new ProjectCPP(name);
 		pr.addGraphMLs(dir);
 
@@ -34,18 +34,19 @@ class ASmellTrackerTest {
 		ASmellTracker tracker = new ASmellTracker(scorer, false);
 
 		PersistenceWriter
-				.register(new SmellSimilarityDataGenerator("test-data/jaccard-scores-antlr-consecutives-only.csv"));
-		// PersistenceWriter.register(new
-		// SmellCharacteristicsGenerator("test-data/smells-characteristics.csv", null));
+				.register(new SmellSimilarityDataGenerator("test-data/jaccard-scores-"+name+"-consecutives-only.csv"));
+		 PersistenceWriter.register(new
+		 SmellCharacteristicsGenerator("test-data/"+name+"-smells-characteristics.csv", pr));
 		// // this test is out of date, added null to allow compilation
 
 		pr.forEach(v -> {
 			logger.info("Tracking version {}- {}", v, v.getGraph());
-			List<ArchitecturalSmell> smells = ArcanDependencyGraphParser.getArchitecturalSmellsIn(v.getGraph());
+			List<ArchitecturalSmell> smells = pr.getArchitecturalSmellsIn(v);
 			smells.forEach(ArchitecturalSmell::calculateCharacteristics);
 			tracker.track(smells, v);
 			PersistenceWriter.sendTo(SmellSimilarityDataGenerator.class, tracker);
 		});
+		PersistenceWriter.sendTo(SmellCharacteristicsGenerator.class, tracker);
 //		int counter = 1;
 //      for (var entry : versionedSystem.entrySet()){
 //          var version = new VersionCPP(Paths.get(dir+entry.getKey()));
